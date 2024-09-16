c53236f00403d0612e338667e7ecafc1ae64f101
alxmart <alxmartinez@gmail.com>
2024-09-16 15:34:01 -0300

    Project created

---
+++ BW01_IAmRich/BW01_IAmRich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BW01_IAmRich
+//
+//  Created by Alessandra Martinez on 16/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
